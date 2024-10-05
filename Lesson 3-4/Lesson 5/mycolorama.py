fc105ff9299544b37aea1683f7ac2ef6ed2b00c1
MihaJunior <yarm.miha@gmail.com>
2024-10-05 16:58:51 +0300

    Work with mycolorama

---
+++ Lesson 3-4/Lesson 5/mycolorama.py
@@ -0,0 +1,12 @@
+import colorama
+
+
+from colorama import Fore, Style, Back
+print(Fore.BLUE + "Hello World")
+print(Back.BLACK + "Hello World")
+print(Style.RESET_ALL)
+print(Style.BRIGHT + 'Hello World')
+print(Style.NORMAL + "Hello World")
+print(Style.DIM + "Hello World")
+print(Style.RESET_ALL)
+print("Hello World")
